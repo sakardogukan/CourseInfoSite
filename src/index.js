64147b92e5b58baae34b444496f8efec34bb30a6
DGKN <sakardogukan@gmail.com>
2024-06-28 23:33:27 +0300

    index.js file updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../src/index.css"
+import "./App.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
