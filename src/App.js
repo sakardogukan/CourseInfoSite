3cafe54f0e32301438efb747ab466a4bbcd56751
DGKN <sakardogukan@gmail.com>
2024-06-29 00:51:40 +0300

    App.js file updated

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Nav from "./components/Nav"
 import Footer from "./components/Footer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./App.scss"
+import "./scss/_reset.scss"
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/instructors" element={<Instructors />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="app-container">
+        <Nav />
+        <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/instructors" element={<Instructors />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <Footer />
+    </div>
     </BrowserRouter >
 
   )
