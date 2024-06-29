fa0ea11c767f4887514a98fe50ccea542be527e8
DGKN <sakardogukan@gmail.com>
2024-06-29 14:45:10 +0300

    Instructors pages updated

--- src/pages/Instructors.jsx
+++ src/pages/Instructors.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Instructors = () => {
   return (
-    <div className='p-3'>Instructors</div>
+    <div className="p-3">
+      <h2>Instructors</h2>
+      <div className="d-flex flex-column">
+        <Link to="1" state={{name:"INS1"}}>Instructors 1</Link>
+        <Link to="2" state={{name:"INS2"}}>Instructors 2</Link>
+        <Link to="3" state={{name:"INS3"}}>Instructors 3</Link>
+      </div>
+    </div>
   )
 }
 
