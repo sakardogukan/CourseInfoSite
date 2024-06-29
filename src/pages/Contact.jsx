a2a2c41cb15ba623b059b19cce045e0f18990411
DGKN <sakardogukan@gmail.com>
2024-06-29 14:44:47 +0300

    Contact pages updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,24 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 const Contact = () => {
-  return <div className="p-3">
-  Contact
-  </div>
+  let navigate = useNavigate()
+
+  return (
+    <div className="p-3">
+      <h1>Contact</h1>
+      <button
+        className="btn btn-success"
+        style={{ marginRight: "10px" }}
+        onClick={() => navigate(-1)}
+      >
+        Go Back
+      </button>
+      <button className="me-2 btn btn-danger" onClick={() => navigate("/")}>
+        Go Home
+      </button>
+    </div>
+  )
 }
 
 export default Contact
