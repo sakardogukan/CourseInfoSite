5172c7d5dcac95c7d9233bb16f8da2d0b2d784b4
DGKN <sakardogukan@gmail.com>
2024-07-01 00:21:42 +0300

    not found pages updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
     <div className="container text-center mt-4">
       <img className="w-75" src={errorImg} alt="" />
       <div>
-        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+        <button onClick={() => navigate("/CourseInfoSite")} className="btn btn-success me-2">
           Home
         </button>
         <button onClick={() => navigate(-1)} className="btn btn-warning">
