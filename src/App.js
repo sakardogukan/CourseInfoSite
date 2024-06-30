db94bdbbd27b1d98ca13ddac3ac3d39470eb89ed
DGKN <sakardogukan@gmail.com>
2024-06-30 17:57:34 +0300

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import Contact from "./pages/Contact"
 import Home from "./pages/Home"
 import Instructors from "./pages/Instructors"
 import NotFound from "./pages/NotFound"
+import Path from "./pages/Path"
 import Nav from "./components/Nav"
 import Footer from "./components/Footer"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import InstructorDetail from "./pages/InstructorDetail"
 import "./App.scss"
 import "./scss/_reset.scss"
+import About from "./components/About"
+import Courses from "./components/Courses"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <Nav />
-        <div className="content">
+
+    <div className="app-container">
+      <Nav />
+      <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} >
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="" element={<About />} /> */}
+            <Route index element={<About />} />
+            <Route path="/courses" element={<Courses />} />
+          </Route>
+          <Route path="/paths" element={<Path />} />
           <Route path="/instructors" element={<Instructors />} />
           <Route path="/instructors/:id" element={<InstructorDetail />} />
           <Route path="/contact" element={<Contact />} />
@@ -25,8 +35,6 @@ function App() {
       </div>
       <Footer />
     </div>
-    </BrowserRouter >
-
   )
 }
 
