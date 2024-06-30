dee5f1045375f2cff25d92695b92f80a6d4e39ba
DGKN <sakardogukan@gmail.com>
2024-06-30 23:29:13 +0300

    aws pages created

---
+++ src/pages/Aws.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Aws = () => {
+  return (
+    <div className='container text-center mt-4'>
+      <h2>AWS-DevOps Path</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, corrupti ducimus explicabo eum adipisci laudantium sint alias, cumque error amet harum! Repellat.</p>
+      <div>
+        <button className='btn btn-outline-danger mt-4'>Learn More</button>
+      </div>
+    </div>
+  )
+}
+
+export default Aws
