a5b0aad59036d6d4654c529ed333039fba63ba3e
DGKN <sakardogukan@gmail.com>
2024-06-30 23:33:39 +0300

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import Contact from "./pages/Contact"
-import Home from "./pages/Home"
-import Instructors from "./pages/Instructors"
-import NotFound from "./pages/NotFound"
-import Path from "./pages/Path"
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-import { Routes, Route } from "react-router-dom"
-import InstructorDetail from "./pages/InstructorDetail"
+import AppRouter from "./Router/AppRouter"
 import "./App.scss"
 import "./scss/_reset.scss"
-import About from "./components/About"
-import Courses from "./components/Courses"
 
 function App() {
   return (
-
-    <div className="app-container">
-      <Nav />
-      <div className="content">
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/" element={<Home />} >
-            {/* <Route path="/about" element={<About />} /> */}
-            {/* <Route path="" element={<About />} /> */}
-            <Route index element={<About />} />
-            <Route path="/courses" element={<Courses />} />
-          </Route>
-          <Route path="/paths" element={<Path />} />
-          <Route path="/instructors" element={<Instructors />} />
-          <Route path="/instructors/:id" element={<InstructorDetail />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-      <Footer />
+    <div>
+      <AppRouter />
     </div>
   )
 }
