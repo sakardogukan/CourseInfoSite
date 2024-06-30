9ffa9b66b394ecaa2f7ba20c3c50fadeebc6f79c
DGKN <sakardogukan@gmail.com>
2024-06-30 17:53:15 +0300

    Home pages updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react"
 import About from "../components/About"
 import Courses from "../components/Courses"
+// import { Outlet } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className='p-3'>
-        <About/>
-        <Courses/>
+    <div className="p-3">
+      <h2>Home Page</h2>
+      <About />
+      <Courses />
+      {/* <Outlet /> */}
     </div>
   )
 }
