9348b59e27c8a59fe7cb49e6687065643cd82a5d
DGKN <sakardogukan@gmail.com>
2024-06-30 23:27:59 +0300

    courses component file updated

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,14 +2,14 @@ import courses from '../data';
 
 const Courses = () => {
   return (
-    <div className="container text-center mt-4 p-4">
-      <h1>OUR COURSES</h1>
+    <div className="container text-center p-4">
+      <h3>OUR COURSES</h3>
       <div className="row justify-content-center gap-4 align-items-center mt-4">
         {courses.map((course) => {
           const { id, img, name, text } = course;
           return (
             <div className="col d-flex justify-content-center" key={id}>
-              <div className="card" style={{ width: '10rem' }}>
+              <div className="card" style={{ width: '10rem', height:'16rem'}}>
                 <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
                   <h5 className="card-title">{name}</h5>
